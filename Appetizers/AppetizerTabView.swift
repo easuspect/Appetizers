6de04a8d4ba76f39254c935126177db406c99fe8
Tolga <tolgatelseren@gmail.com>
2024-10-07 10:23:02 -0700

    Initial Commit

---
+++ Appetizers/AppetizerTabView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Appetizers
+//
+//  Created by Tolga Telseren on 10/7/24.
+//
+
+import SwiftUI
+
+struct AppetizerTabView: View {
+    var body: some View {
+        TabView {
+            Text("Hello, world!")
+                .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerTabView()
+    }
+}
---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Tolga Telseren on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppetizerTabView()
+        }
+    }
+}
